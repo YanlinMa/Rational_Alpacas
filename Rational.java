70cebf741c917ac06f04952d0db28c0ae2ec1703
YanlinMa <yma3@stuy.edu>
2015-11-18 10:25:57 -0500

    modified header

--- Rational.java
+++ Rational.java
@@ -1,6 +1,6 @@
-//Team JackSkellington- Grace Stempel, Andrea Ma
+//Team NightmareBeforeChristmas- Andrea Ma, Sarah Yoon
 //APCS1 pd10
-//HW32- Irrationality Stops Here
+//HW33- Do You Even Add, Bro?
 //2015-11-18
 
 public class Rational {
